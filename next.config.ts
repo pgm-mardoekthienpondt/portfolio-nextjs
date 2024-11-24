62057a0566acfc393f05db9cc669501e73c855d3
Mardoek Thienpondt <mardoek.thienpondt@student.arteveldehs.be>
2024-11-24 20:06:11 +0100

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/portfolio-nextjs",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
